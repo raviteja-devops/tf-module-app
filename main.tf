b1b76562e097f531fe04bc573d242cbc8c47d499
raviteja <ravitejatfcdemo@gmail.com>
2023-02-22 11:30:43 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,23 +1,12 @@
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet-group"
-  subnet_ids = var.subnet_ids
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-subnet-group" }
-  )
-}
-
-
 resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb-security-group"
-  description = "${var.env}-docdb-security-group"
+  name        = "${var.env}-${var.component}-security-group"
+  description = "${var.env}-${var.component}-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
     description      = "MongoDB"
-    from_port        = 27017
-    to_port          = 27017
+    from_port        = var.app_port
+    to_port          = var.app_port
     protocol         = "tcp"
     cidr_blocks      = var.allow_cidr
   }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,5 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
+variable "component" {}
+variable "app_port" {}
